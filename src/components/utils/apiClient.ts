0c8d62bf3c87968948ff3993b38e99796eed88dc
Glory <ayanwolaglory@gmail.com>
2025-10-22 18:22:48 +0100

    update api client to use /api for render backend deployment

--- src/components/utils/apiClient.ts
+++ src/components/utils/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api",
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api` || "http://localhost:8000/api",
     headers: {
         "Accept": "application/json",
         "Content-Type": "application/json",
